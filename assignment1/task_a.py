32e156a60a9d7090ac23cd1abc49c63e01f42ed6
RobertZetterlund <31474146+RobertZetterlund@users.noreply.github.com>
2020-09-01 12:15:32 +0200

    add task_a using pandas and numpy

---
+++ assignment1/task_a.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+SELECTED_YEAR = 2010
+HEADER_TITLE = 'Life expectancy (years)'
+
+df = pd.read_csv('life-expectancy.csv')
+
+# Extract data entries from dataframe where year is SELECTED YEAR, essentially filter
+entries_from_year = df[(df['Year'] == SELECTED_YEAR)]
+
+# Calculate standard deviation and mean
+life_std = np.std(entries_from_year[HEADER_TITLE])
+life_mean = np.mean(entries_from_year[HEADER_TITLE])
+
+# get countries one mean above average
+top_percentage_countries = entries_from_year[entries_from_year[HEADER_TITLE] > life_mean+life_std]
+
+#print countires being one std above the mean
+print(top_percentage_countries['Entity'])
