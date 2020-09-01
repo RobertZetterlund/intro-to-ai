fdc3d639d974a493f10ec25c8bff346ad799878e
RobertZetterlund <31474146+RobertZetterlund@users.noreply.github.com>
2020-09-01 13:00:07 +0200

    add task_b

--- assignment1/task_b.py
+++ assignment1/task_b.py
@@ -1,20 +1,52 @@
 import numpy as np
 import pandas as pd
+import sys
 
+# For the sake of the assignment,
+# lets assume that high and low represents is based on the standard deviation.
+# To which extent can be set by adjusting the variable STD_CONSTANT below
+
+
+STD_CONSTANT = 0.5
+
+# Allow for argument in unix. eg. 'python task_b.py 0.2'
+if(sys.argv[1]):
+    STD_CONSTANT = float(sys.argv[1])
+
+# Setup constants
 SELECTED_YEAR = 2010
-TITLE_OF_RELEVANT_DATA = 'Life expectancy (years)'
+HEADER_TITLE_GDP = 'Output-side real GDP per capita (2011 international-$)'
+HEADER_TITLE_LIFE = 'Life expectancy (years)'
 
-df = pd.read_csv('life-expectancy.csv')
+# use pandas to read csv
+df_gdp = pd.read_csv('gdp.csv')
+df_life = pd.read_csv('life-expectancy.csv')
 
-# Extract data entries from dataframe where year is SELECTED YEAR, essentially filter
-life_exp_list = df[(df['Year'] == SELECTED_YEAR)]
+# filter data on selected year
+gdp_entries = df_gdp[(df_gdp['Year'] == SELECTED_YEAR)]
+life_entries = df_life[(df_life['Year'] == SELECTED_YEAR)]
 
-# Calculate standard deviation and mean
-life_std = np.std(life_exp_list[TITLE_OF_RELEVANT_DATA])
-life_mean = np.mean(life_exp_list[TITLE_OF_RELEVANT_DATA])
+# merge entries with inner join. Excluding entities not available in both datasets.
+merged_entries = pd.merge(gdp_entries, life_entries,
+                          on=['Code', 'Year', 'Entity'])
 
-# get countries one mean above average
-top_percentage_countries = life_exp_list[life_exp_list[TITLE_OF_RELEVANT_DATA] > life_mean+life_std]
+# get standard deviation and mean from entries
+gdp_std = np.std(merged_entries[HEADER_TITLE_GDP])
+gdp_mean = np.mean(merged_entries[HEADER_TITLE_GDP])
 
-#print countires being one std above the mean
-print(top_percentage_countries['Entity'])
+life_std = np.std(merged_entries[HEADER_TITLE_LIFE])
+life_mean = np.mean(merged_entries[HEADER_TITLE_LIFE])
+
+# filter based on having high life-expectancy
+high_life = merged_entries[merged_entries[HEADER_TITLE_LIFE]
+                           > life_mean + life_std * STD_CONSTANT]
+# filter based on having low gdp
+high_life_low_gdp = high_life[high_life[HEADER_TITLE_GDP]
+                              < gdp_mean - gdp_std * STD_CONSTANT]
+
+# print countries having a high life expectancy and a low gdp
+if(len(high_life_low_gdp) > 0):
+    print('These countries have high life expectancy and a low gdp \n')
+    print(high_life_low_gdp['Entity'])
+else:
+    print('There are no countries with a high life expectancy and a low gdp')
