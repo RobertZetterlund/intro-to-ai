744844d7cb0e1a6c62f3e30a9c4d339523c8d2bb
Robert <Robert.Zetterlund@outlook.com>
2020-09-01 12:34:19 +0200

    Update task_a.py

--- assignment1/task_a.py
+++ assignment1/task_a.py
@@ -13,7 +13,7 @@ entries_from_year = df[(df['Year'] == SELECTED_YEAR)]
 life_std = np.std(entries_from_year[HEADER_TITLE])
 life_mean = np.mean(entries_from_year[HEADER_TITLE])
 
-# get countries one mean above average
+# get countries one std above average
 top_countries = entries_from_year[entries_from_year[HEADER_TITLE]
                                   > life_mean+life_std]
 
