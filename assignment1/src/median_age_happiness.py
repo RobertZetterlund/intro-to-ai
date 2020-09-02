e6f8dbe58248a23ceb85bffc3cfb4c846a975b41
RobertZetterlund <31474146+RobertZetterlund@users.noreply.github.com>
2020-09-02 19:21:05 +0200

    add scatterplot median age vs happiness

---
+++ assignment1/src/median_age_happiness.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+
+# Setup constants
+SELECTED_YEAR = 2010
+HEADER_TITLE_X = "UN Population Division (Median Age) (2017) (years)"
+HEADER_TITLE_Y = "World Happiness Report 2016 (Cantril Ladder (0=worst; 10=best))"
+
+# use pandas to read csv
+df_x = pd.read_csv("../res/median-age.csv")
+dy_y = pd.read_csv("../res/happiness-cantril-ladder.csv")
+
+# filter data on selected year
+x_entries = df_x[(df_x["Year"] == SELECTED_YEAR)]
+y_entries = dy_y[(dy_y["Year"] == SELECTED_YEAR)]
+
+# merge entries with inner join. Excluding entities not available in both datasets.
+merged_entries = pd.merge(x_entries, y_entries, on=["Code", "Year", "Entity"])
+
+
+# setup scatterplot
+fig, ax = plt.subplots()
+# scatterplot gdp as x, life-expectancy as y
+plt.scatter(merged_entries[HEADER_TITLE_X], merged_entries[HEADER_TITLE_Y])
+plt.xlabel(HEADER_TITLE_X)
+plt.ylabel(HEADER_TITLE_Y)
+plt.savefig("../fig/median-age_happiness.png")
