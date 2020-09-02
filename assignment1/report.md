6f798561578a887440b6a39fe642d822afdf7215
RobertZetterlund <31474146+RobertZetterlund@users.noreply.github.com>
2020-09-02 09:42:58 +0200

    add task_d

---
+++ assignment1/report.md
@@ -0,0 +1,13 @@
+# Report
+
+
+### State the Assumptions and decisions
+
+ * We selected year 2010
+ * 
+
+
+
+
+## Task C
+We look at the scatterplot and arbitrariry 
---
+++ assignment1/task_d.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+
+# Setup constants
+SELECTED_YEAR = 2010
+HEADER_TITLE_GDP = 'Output-side real GDP per capita (2011 international-$)'
+HEADER_TITLE_LIFE = 'Life expectancy (years)'
+
+# use pandas to read csv
+df_gdp = pd.read_csv('gdp.csv')
+df_life = pd.read_csv('life-expectancy.csv')
+
+# filter data on selected year
+gdp_entries = df_gdp[(df_gdp['Year'] == SELECTED_YEAR)]
+life_entries = df_life[(df_life['Year'] == SELECTED_YEAR)]
+
+# merge entries with inner join. Excluding entities not available in both datasets.
+merged_entries = pd.merge(gdp_entries, life_entries, on=['Code', 'Year', 'Entity'])
+
+df_clean = merged_entries.drop(columns=['Code', 'Year'])
+
+df_clean.to_csv('clean.csv', index=False)
