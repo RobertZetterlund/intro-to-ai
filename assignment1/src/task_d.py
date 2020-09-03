8ccb42eeaa9c74597b681b8e33d38d9e8cdcda52
RobertZetterlund <31474146+RobertZetterlund@users.noreply.github.com>
2020-09-03 20:57:05 +0200

    more clean

--- assignment1/src/task_d.py
+++ assignment1/src/task_d.py
@@ -8,8 +8,8 @@ GDP = "Output-side real GDP per capita (2011 international-$)"
 LIFE = "Life expectancy (years)"
 
 # use pandas to read csv
-df_gdp = pd.read_csv("gdp.csv")
-df_life = pd.read_csv("life-expectancy.csv")
+df_gdp = pd.read_csv("../res/gdp.csv")
+df_life = pd.read_csv("../res/life-expectancy.csv")
 
 # filter data on selected year
 gdp_entries = df_gdp[(df_gdp["Year"] == SELECTED_YEAR)]
@@ -19,5 +19,8 @@ life_entries = df_life[(df_life["Year"] == SELECTED_YEAR)]
 merged_entries = pd.merge(gdp_entries, life_entries, on=["Code", "Year", "Entity"])
 
 df_clean = merged_entries.drop(columns=["Code", "Year"])
+df_clean = df_clean.rename(columns={GDP: "GDP (2011 international-$)"})
+
+print(df_clean.head(4))
 
 df_clean.to_csv("../res/clean.csv", index=False)
