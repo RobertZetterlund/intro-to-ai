d7606b86e60233c9eb4e16b4e19f575eac02e36c
Tobias Lindroth <31400061+rothen98@users.noreply.github.com>
2020-09-17 09:29:21 +0200

    elbow plot

--- assignment3/src/kmeans.py
+++ assignment3/src/kmeans.py
@@ -20,10 +20,28 @@ random_state = 170
 
 y_pred = KMeans(n_clusters=4, random_state=random_state).fit_predict(X)
 
-getColors = lambda p: colors[p]
-vColors = np.vectorize(getColors)
 
-df.plot.scatter(x=PHI, y=PSI, c=vColors(y_pred))
-plt.yticks(np.arange(-180, 181, 40))
-plt.xticks(np.arange(-180, 181, 40))
+
+distorsions = []
+for k in range(2, 10):
+    kmeans = KMeans(n_clusters=k)
+    kmeans.fit(X)
+    #inertia is sum of squared distances of samples to their closest cluster center.
+
+    distorsions.append(kmeans.inertia_)
+
+fig = plt.figure(figsize=(15, 5))
+plt.plot(range(2, 10), distorsions)
+plt.grid(True)
+plt.title('Elbow curve')
+
+#getColors = lambda p: colors[p]
+#vColors = np.vectorize(getColors)
+
+
+
+
+#df.plot.scatter(x=PHI, y=PSI, c=vColors(y_pred))
+#plt.yticks(np.arange(-180, 181, 40))
+#plt.xticks(np.arange(-180, 181, 40))
 plt.show()
