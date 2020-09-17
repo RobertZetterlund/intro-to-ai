87ecc490325258a919fb9372b676dbc8ff41c40e
RobertZetterlund <robert.zetterlund@outlook.com>
2020-09-17 09:25:15 +0200

    add cooler heatmap

---
+++ assignment3/src/cooler-heatmap.py
@@ -0,0 +1,21 @@
+# Author: {Tobias Lindroth & Robert Zetterlund}
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set_theme()
+import numpy as np
+
+
+# Setup constants
+PATH = '../res/data_all.csv'
+PHI = "phi"
+PSI = "psi"
+
+
+
+# use pandas to read csv
+df = pd.read_csv(PATH)
+sns.kdeplot(x=df[PHI], y=df[PSI], fill=True, cmap="mako")
+#df.plot.scatter(x=PHI, y=PSI)
+plt.yticks(np.arange(-180, 181, 40))
+plt.xticks(np.arange(-180, 181, 40))
+plt.show()
