a19d8b6c6936f6ae8659c1b566e39c6ac0124669
Tobias Lindroth <31400061+rothen98@users.noreply.github.com>
2020-09-18 11:14:32 +0200

    Added comments

--- assignment3/src/dbscan/dbscan-eps.py
+++ assignment3/src/dbscan/dbscan-eps.py
@@ -15,12 +15,19 @@ df[PSI] = df[PSI].apply(lambda psi: psi + 360 if psi < -100 else psi)
 
 X = df[[PHI, PSI]]
 
-neigh = NearestNeighbors(n_neighbors=50)
+neigh = NearestNeighbors(n_neighbors=200)
 nbrs = neigh.fit(X)
+
+# Get distances to the closest n_neighbors neighbors for each node
 distances, indices = nbrs.kneighbors(X)
 
+# Sort each node's distances to its closest n_neighbors neighbors
 distances = np.sort(distances, axis=0)
+
+# For each node, pick out the distance to the neighbor (out of closest n_neighbors) that is furthest away. 
 distances = distances[:,1]
+
+#Plot these distances
 plt.plot(distances)
 
 plt.show()
