8ea5bdc72d2d779d8111fe796d28bed87884470d
RobertZetterlund <robert.zetterlund@outlook.com>
2020-09-23 21:55:12 +0200

    add links to pages

---
+++ assignment4/src/naive_bayes.py
@@ -0,0 +1,2 @@
+# mutli: https://scikit-learn.org/stable/modules/naive_bayes.html#multinomial-naive-bayes
+# bernoulli:  https://scikit-learn.org/stable/modules/naive_bayes.html#bernoulli-naive-bayes
