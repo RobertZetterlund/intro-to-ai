b9f0e99d24d47fd1fdee973276103c6a62415a78
RobertZetterlund <robert.zetterlund@outlook.com>
2020-10-02 14:33:16 +0200

    remove v_new

--- assignment5/chain_mdp.py
+++ assignment5/chain_mdp.py
@@ -5,8 +5,8 @@ env = gym.make('NChain-v0')
 
 Q = np.zeros((env.observation_space.n, env.action_space.n))
 
-V_prev = np.copy(Q)
-V_curr = np.ones_like(Q)
+v_prev = np.copy(Q)
+v_curr = np.ones_like(Q)
 
 rewards = np.array([
     [0, 2],
@@ -23,19 +23,14 @@ state_space_size = env.observation_space.n
 alfa = 0.8
 epsilon = 0.9
 
-V_new = np.copy(V_curr)
-
-while not np.allclose(V_prev, V_curr, atol=0.001):
-    V_new = np.copy(V_curr)
-    V_prev = np.copy(V_curr)
+while not np.allclose(v_prev, v_curr, atol=0.001):
+    v_prev = np.copy(v_curr)
     for state in range(state_space_size):
         for action in range(action_space_size):
             not_action = abs(action-1)
 
-            V_new[state][action] = alfa * (rewards[state][action] + epsilon*V_prev[state][action]) + (
-                1-alfa) * (rewards[state][not_action] + epsilon * V_prev[state][action])
-
-    v_curr = np.copy(V_new)
+            v_curr[state][action] = alfa * (rewards[state][action] + epsilon*v_prev[state][action]) + (
+                1-alfa) * (rewards[state][not_action] + epsilon * v_prev[state][action])
     
 print("\n******** Candidate solution ********\n")
 print(v_curr)
