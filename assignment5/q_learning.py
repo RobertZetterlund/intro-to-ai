42f17c0bd0b77683c3f90a857c0ce2c1c1e47edc
Tobias Lindroth <31400061+rothen98@users.noreply.github.com>
2020-10-03 14:03:59 +0200

    wip q_learning

--- assignment5/q_learning.py
+++ assignment5/q_learning.py
@@ -1,13 +1,14 @@
 import gym
 import pandas as pd
 import numpy as np
+import random
 env = gym.make('NChain-v0')
 
 Q = np.zeros((env.observation_space.n, env.action_space.n))
 
 
 #
-#epsilon = 0.9
+epsilon = 0.1
 # learning rate
 alfa = 0.1
 # discount factor
@@ -18,25 +19,28 @@ actionDict = {
     1: "Backward"
 }
 
-for i_episode in range(100000):
+for i_episode in range(1000):
     old_state = env.reset()
+    done = False
+    while not done:
+        if random.uniform(0, 1) < epsilon:
+            action = env.action_space.sample() # Explore action space
+        else:
+            action = np.argmax(Q[old_state]) # Exploit learned values
 
-    for t in range(5):
         action = env.action_space.sample()
         new_state, reward, done, info = env.step(action)
 
+    
         # given performing action from state observation, we get reward... so
         #move = actionDict[action]
         #print("prevstate: ", prev_state, " move: ", move, "state: ", state, " reward: ", reward)
-
         Q[old_state][action] = Q[old_state][action] + alfa * \
             (reward + gamma * max(Q[new_state, :]) - Q[old_state][action])
 
         old_state = new_state
 
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
-            break
+        
 
 print(Q)
 # env.close()
