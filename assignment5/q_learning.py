6dff33d972776bebe246228b7d49552017a5431d
RobertZetterlund <robert.zetterlund@outlook.com>
2020-10-03 16:47:53 +0200

    honestly i dont even know anymore

--- assignment5/q_learning.py
+++ assignment5/q_learning.py
@@ -2,21 +2,22 @@ import gym
 import pandas as pd
 import numpy as np
 # init environment with slip = 0
-env = gym.make('NChain-v0', slip=0)
+env = gym.make('NChain-v0', slip=0.2)
 
 Q = np.zeros((env.observation_space.n, env.action_space.n))
+
+new_Q = np.ones_like(Q)
 
 # learning rate / also slip chance
 allowed_steps = 100
 nr_episodes = 1000
 
-alfa = 0.2
+alfa = 0.1
 # discount factor
 gamma = 0.95
 
-for i_episode in range(nr_episodes):
+for episode in range(nr_episodes):
     old_state = env.reset()
-
     # how many steps are the agent allowed to go?
     for t in range(allowed_steps):
         # Get best action based on Q matrix
@@ -24,10 +25,12 @@ for i_episode in range(nr_episodes):
         # Step with that action
         new_state, reward, done, info = env.step(action)
         # update Q value for old state following policy
-        Q[old_state][action] = (1-alfa) * Q[old_state][action] + alfa * \
+        Q[old_state][action] += alfa * \
             (reward + gamma * max(Q[new_state, :]) - Q[old_state][action])
         
         old_state = new_state
+    
+    
 
 
 print("\n******** Q solution ********\n")
