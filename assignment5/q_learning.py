0010afc4fd0c1218c61075078af8ad43de735ebc
RobertZetterlund <robert.zetterlund@outlook.com>
2020-10-03 13:27:03 +0200

    add choosing best action

--- assignment5/q_learning.py
+++ assignment5/q_learning.py
@@ -4,8 +4,6 @@ import numpy as np
 env = gym.make('NChain-v0')
 
 Q = np.zeros((env.observation_space.n, env.action_space.n))
-
-
 #
 #epsilon = 0.9
 # learning rate
@@ -13,30 +11,26 @@ alfa = 0.1
 # discount factor
 gamma = 0.95
 
-actionDict = {
-    0: "Forward",
-    1: "Backward"
-}
-
-for i_episode in range(100000):
+for i_episode in range(10000):
     old_state = env.reset()
 
-    for t in range(5):
-        action = env.action_space.sample()
+    for t in range(10):
+        # Get best action based on Q matrix
+        action = np.argmax(Q[old_state, :])
+        # Step with that action
         new_state, reward, done, info = env.step(action)
-
-        # given performing action from state observation, we get reward... so
-        #move = actionDict[action]
-        #print("prevstate: ", prev_state, " move: ", move, "state: ", state, " reward: ", reward)
-
+        # update Q value for old state following policy
         Q[old_state][action] = Q[old_state][action] + alfa * \
             (reward + gamma * max(Q[new_state, :]) - Q[old_state][action])
-
+        
         old_state = new_state
 
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
-            break
 
-print(Q)
+print("\n******** Q solution ********\n")
+print("Q = \n", Q)
+
+print("\n******** Policy ********************")
+print("(forward,backward) = (0,1)\n")
+
+print(np.argmax(Q, axis=1), "\n")
 # env.close()
