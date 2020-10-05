eb2958df727ed20c53def89b436b5d7a74673028
Tobias Lindroth <31400061+rothen98@users.noreply.github.com>
2020-10-05 11:35:58 +0200

    Improve q3 and q4

--- assignment5/q_learning.py
+++ assignment5/q_learning.py
@@ -1,35 +1,42 @@
 import gym
 import pandas as pd
 import numpy as np
-env = gym.make('NChain-v0')
+import random
+env = gym.make('NChain-v0', slip=0.2)
 
 Q = np.zeros((env.observation_space.n, env.action_space.n))
-
-new_Q = np.ones_like(Q)
-
-# learning rate / also slip chance
-allowed_steps = 100
-nr_episodes = 1000
-
+Qcopy = np.ones((env.observation_space.n, env.action_space.n))
+#
+epsilon = 0.5
+# learning rate
 alfa = 0.1
 # discount factor
 gamma = 0.95
 
-for episode in range(nr_episodes):
+iteration = 0
+# To make the algorithm converge
+alfadecay = 0.999
+while not np.allclose(Q,Qcopy, atol=0.01):
+    iteration += 1
     old_state = env.reset()
-    # how many steps are the agent allowed to go?
-    for t in range(allowed_steps):
-        # Get best action based on Q matrix
-        action = np.argmax(Q[old_state, :])
-        # Step with that action
+    alfa *= alfadecay
+    done = False
+    Qcopy = Q.copy() 
+    while not done:
+        action = env.action_space.sample() if random.random() < epsilon else np.argmax(Q[old_state,:])
+    
         new_state, reward, done, info = env.step(action)
         # update Q value for old state following policy
+
         Q[old_state][action] += alfa * \
             (reward + gamma * max(Q[new_state, :]) - Q[old_state][action])
         
         old_state = new_state
-    
-    
+
+    if(iteration % 100 == 0):
+        diff = np.subtract(Q, Qcopy)
+        diff = np.absolute(diff)
+        print("Iteration", iteration, "Max diff: ", diff.max())
 
 
 print("\n******** Q solution ********\n")
@@ -39,3 +46,4 @@ print("\n******** Policy ********************")
 print("(forward,backward) = (0,1)\n")
 
 print(np.argmax(Q, axis=1), "\n")
+# env.close()
